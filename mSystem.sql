32a4c96dfe137b0bb85ee0ada61ff0c6ac63827e
Marco Aurélio <marcofilho_1@hotmail.com>
2016-01-21 21:16:56 -0200

    Alteração SQL

--- mSystem.sql
+++ mSystem.sql
@@ -1,23 +1,60 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+-- phpMyAdmin SQL Dump
+-- version 4.4.14
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:3306
+-- Generation Time: Jan 21, 2016 at 03:13 PM
+-- Server version: 10.0.20-MariaDB
+-- PHP Version: 5.6.13
 
-CREATE SCHEMA IF NOT EXISTS `mSystem` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
-USE `mSystem` ;
-
--- -----------------------------------------------------
--- Table `mSystem`.`keepsystem`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mSystem`.`keepsystem` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `description` VARCHAR(100) NOT NULL,
-  `initial` VARCHAR(10) NOT NULL,
-  `email` VARCHAR(100) NULL,
-  `url` VARCHAR(50) NULL,
-  PRIMARY KEY (`id`))
-ENGINE = InnoDB;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
 
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `m_system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `keepsystem`
+--
+
+CREATE TABLE IF NOT EXISTS `keepsystem` (
+  `id` int(11) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `initial` varchar(10) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `url` varchar(50) DEFAULT NULL,
+  `status` int(1) NOT NULL DEFAULT '1'
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `keepsystem`--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `keepsystem`
+--
+ALTER TABLE `keepsystem`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `keepsystem`
+--
+ALTER TABLE `keepsystem`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
